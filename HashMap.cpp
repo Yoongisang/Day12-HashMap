f745b81bd96c85b786746c7f3e47c82c65ff043e
Yoongisang <sawsd@naver.com>
2025-03-30 23:24:38 +0900

    Update HashMap.cpp

--- HashMap.cpp
+++ HashMap.cpp
@@ -1,34 +1,100 @@
 #include "HashMap.h"
 
-void HashMap::Add(int key, int value)
+void HashMap::Add(int _key, int _value)
 {
+    if (Count() == TABLE_SIZE)
+    {
+        cout << "가득 찼습니다." << endl;
+        return;
+    }
+    else
+    {
+        Node* NewNode = new Node;
+        NewNode->key = _key;
+        NewNode->data = _value;
+        int i = _key % TABLE_SIZE;
+        if (table[i] == nullptr)
+        {
+            table[i] = NewNode;
+        }
+        else
+        {
+            while (table[i] != nullptr)
+            {
+                i++;
+                i %= TABLE_SIZE;
+            }
+            table[i] = NewNode;
+        }
+        count++;
+    }
     
 }
 
-int HashMap::GetValue(int key)
+int HashMap::GetValue(int _key)
 {
-    return 0;
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (_key == table[i]->key)
+        {
+            cout << _key << "의 Value값: " << table[i]->data << " 출력" << endl;
+            return table[i]->data;
+        }
+    }
+    cout << "값이 없습니다." << endl;
+    return -99;
 }
 
-void HashMap::Remove(int key)
+void HashMap::Remove(int _key)
 {
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (_key == table[i]->key)
+        {
+            delete table[i];
+            table[i] == nullptr;
+        }
+    }
 }
 
 int HashMap::Count()
 {
-    return 0;
+    int count = 0;
+
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (table[i] != nullptr)
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 void HashMap::Clear()
 {
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        if (table[i] != nullptr)
+        {
+            delete table[i];
+            table[i] = nullptr;
+        }
+    }
 }
 
 HashMap::HashMap()
 {
-    table = nullptr;
+    Node* slot[TABLE_SIZE];
+    table = slot;
+    for (int i = 0; i < TABLE_SIZE; i++)
+    {
+        table[i] = nullptr;
+    }
     count = 0;
 }
 
 HashMap::~HashMap()
 {
+    Clear();
 }
