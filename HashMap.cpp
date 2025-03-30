75edb5f218f8ed535dc95d32e5eab47222e473b0
Yoongisang <sawsd@naver.com>
2025-03-30 22:27:20 +0900

    Initial commit

---
+++ HashMap.cpp
@@ -0,0 +1,33 @@
+#include "HashMap.h"
+
+void HashMap::Add(int key, int value)
+{
+    
+}
+
+int HashMap::GetValue(int key)
+{
+    return 0;
+}
+
+void HashMap::Remove(int key)
+{
+}
+
+int HashMap::Count()
+{
+    return 0;
+}
+
+void HashMap::Clear()
+{
+}
+
+HashMap::HashMap()
+{
+    table = nullptr;
+}
+
+HashMap::~HashMap()
+{
+}
